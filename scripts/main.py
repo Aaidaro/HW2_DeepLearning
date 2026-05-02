e9d0195e5ef184b008dcc0e8325667c5e3d0d3f5
Aaidaro <aaidaroshani@gmail.com>
2026-05-02 23:03:41 +0330

    Minor changes

--- scripts/main.py
+++ scripts/main.py
@@ -80,7 +80,7 @@ def parse_args():
     parser.add_argument(
         "--save-dir",
         type=str,
-        default="/models/saved_models"
+        default="models/saved_models"
     )
 
     parser.add_argument(
